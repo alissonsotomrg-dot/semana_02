4956daa0836977a72780df5c5da2a85012dcfb9e
{Alisson} <{alissonsotomrg@gmail.com}>
2026-09-17 17:39:32 -0600

    trabajando con variables

--- variables.py
+++ variables.py
@@ -1,3 +1,6 @@
 # Esta sesion será para aprender el uso de variables en Phyton
-print("Hola mundo")
-print("hoy es miércoles")
+print("Sistema de mensajeria")
+distancia_km = 6.5
+peso_kg = 2.0
+es_urgente = True
+zona = "B"
